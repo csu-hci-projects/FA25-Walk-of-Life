968f8e2954d57ec38a2594164a5490f50bee1c07
KJ-Jacobs <K1jacobs@colostate.edu>
2025-11-20 14:55:30 -0700

    Added Animation

--- Assets/Scripts/Interactions/Door.cs
+++ Assets/Scripts/Interactions/Door.cs
@@ -6,10 +6,25 @@ public class Door : MonoBehaviour, IInteractable
 
     public string interactionPrompt => prompt;
 
+    [SerializeField] private GameObject door;
+    [SerializeField] private float openRot, speed, closeRot;
+
+    private bool isOpening = false;
 
     public bool Interact(Interactor interactor)
     {
-        gameObject.SetActive(false);
+        // Toggle the door state
+        isOpening = !isOpening;
         return true;
+    }
+
+    private void Update()
+    {
+        Vector3 currentRot = door.transform.localEulerAngles;
+        float targetRotation = isOpening ? openRot : closeRot;
+
+        float newY = Mathf.LerpAngle(currentRot.y, targetRotation, speed * Time.deltaTime);
+
+        door.transform.localEulerAngles = new Vector3(currentRot.x, newY, currentRot.z);
     }
 }
