76bff4dc924b32bd9880e2978b99529a838d5949
wwardlow <157065670+wwardlow@users.noreply.github.com>
2025-11-20 13:39:17 -0700

    sound effects for the nightmares

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -10,7 +10,24 @@ public class EnemyAI : MonoBehaviour
     public float moveSpeed = 3f;
     public float stopDistance = 1.5f;
     public float rotationSpeed = 5f;  
-    public float attackDistance = 2f; 
+    public float attackDistance = 2f;
+
+    [Header("Sound Effects")]
+
+    public AudioSource audioSource;
+    public AudioClip[] attackSounds;
+    public AudioClip[] deathSounds;
+    public AudioClip[] hurtSounds;
+    public AudioClip[] idleSounds;
+    public AudioClip[] walkSounds;
+
+    public float idleCooldown = 3f;
+    public float idleTimer = 0f;
+
+    public float footCooldown = 3f;
+    public float footTimer = 0f;
+    public float attackCooldown = 3f;
+    public float attackTimer = 0f;
 
     [HideInInspector]
     public bool seesPlayer = false;
@@ -25,9 +42,17 @@ public class EnemyAI : MonoBehaviour
 
     void Update()
     {
+        idleTimer -= Time.deltaTime;
+        footTimer -= Time.deltaTime;
+        attackTimer -= Time.deltaTime;
         if (!seesPlayer)
         {
             animator.SetBool("SeesPlayer", false);
+            if(idleTimer <= 0f)
+            {
+                PlayRandomSound(idleSounds);
+                idleTimer = idleCooldown;
+            }
             return;
         }
 
@@ -37,7 +62,16 @@ public class EnemyAI : MonoBehaviour
 
         animator.SetFloat("DistancePlayer", distance);
         animator.SetBool("SeesPlayer", true);
-
+        if (distance > stopDistance && footTimer <= 0f)
+        {
+            PlayRandomSound(walkSounds);
+            footTimer = footCooldown;
+        }
+        if (distance <= attackDistance && attackTimer <= 0f)
+        {
+            PlayRandomSound(attackSounds);
+            attackTimer = attackCooldown;
+        }
         if (distance > stopDistance)
         {
             Vector3 direction = (flatPlayerPos - flatEnemyPos).normalized;
@@ -57,5 +91,15 @@ public class EnemyAI : MonoBehaviour
     public void PlayerDetected()
     {
         seesPlayer = true;
+    }
+
+    public void PlayRandomSound(AudioClip[] clips)
+    {
+        if (clips.Length == 0)
+        {
+            return;
+        }
+        AudioClip clip = clips[Random.Range(0, clips.Length)];
+        audioSource.PlayOneShot(clip);
     }
 }
