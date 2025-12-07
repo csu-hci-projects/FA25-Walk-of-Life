470736d0779d68de24c96b195d89748a753689e4
Nathan <xdnathanm@gmail.com>
2025-12-07 12:22:42 -0700

    Fixed the boss trigger and hid the mouse

--- Assets/Scripts/HideCursor.cs
+++ Assets/Scripts/HideCursor.cs
@@ -5,20 +5,26 @@ public class HideCursor : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
+        Scene currentScene = SceneManager.GetActiveScene();
+       
+        if (currentScene.name != "stage1")
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         Scene currentScene = SceneManager.GetActiveScene();
-        if (currentScene.name != "Stage1")
+        
+        if (currentScene.name != "stage1")
         {
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
         }
-        else
+        else if(currentScene.name == "stage1")
         {
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
